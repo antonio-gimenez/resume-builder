a469a3dff8d3c379537e031683027d4befe7191f
Antonio Gimenez <grupocapgemini-services.antonio.gimenez@hp.com>
2022-11-21 19:06:04 +0100

    Portaled Modal to modal-root

--- src/components/Modal/Modal.jsx
+++ src/components/Modal/Modal.jsx
@@ -1,13 +1,18 @@
 import React from "react";
+import { createPortal } from "react-dom";
+
+const modalRoot = document.getElementById("modal-root");
 
 function Modal({ children, open = false, onClose }) {
   if (!open) return null;
 
-  return (
+  const modalWrapper = (
     <div className="modal-wrapper">
       <div className="modal">{children}</div>
     </div>
   );
+
+  return createPortal(modalWrapper, modalRoot);
 }
 
 export default Modal;
