b9e64c478035c7a095c2e0c267f58dd9eec75217
Antonio Gimenez <58783747+antonio-gimenez@users.noreply.github.com>
2022-12-14 02:32:37 +0100

    divide

--- src/components/resume/PersonalDetails.jsx
+++ src/components/resume/PersonalDetails.jsx
@@ -15,64 +15,66 @@ function PersonalDetails() {
         <h3>Personal Details</h3>
       </CardHeader>
       <CardContent>
-        <Input
-          id="firstName"
-          placeholder={"ex: John"}
-          label={"First Name"}
-          value={profile.firstName}
-          onChange={(e) => updateProfile(e)}
-          name="firstName"
-        />
-        <Input
-          id="lastName"
-          placeholder={"ex: Doe"}
-          label={"Last Name"}
-          value={profile.lastName}
-          onChange={(e) => updateProfile(e)}
-          name="lastName"
-        />
-        <ProfileImage />
+        <div className="container">
+          <Input
+            id="firstName"
+            placeholder={"ex: John"}
+            label={"First Name"}
+            value={profile.firstName}
+            onChange={(e) => updateProfile(e)}
+            name="firstName"
+          />
+          <Input
+            id="lastName"
+            placeholder={"ex: Doe"}
+            label={"Last Name"}
+            value={profile.lastName}
+            onChange={(e) => updateProfile(e)}
+            name="lastName"
+          />
+          <ProfileImage />
 
-        <Input
-          id="email"
-          placeholder={"ex: john@doe.com"}
-          label={"Email"}
-          value={profile.email}
-          onChange={(e) => updateProfile(e)}
-          name="email"
-        />
-        <Input
-          id="phone"
-          placeholder={"ex: 123-456-7890"}
-          label={"Phone"}
-          value={profile.phone}
-          onChange={(e) => updateProfile(e)}
-          name="phone"
-        />
+          <Input
+            id="email"
+            placeholder={"ex: john@doe.com"}
+            label={"Email"}
+            value={profile.email}
+            onChange={(e) => updateProfile(e)}
+            name="email"
+          />
+          <Input
+            id="phone"
+            placeholder={"ex: 123-456-7890"}
+            label={"Phone"}
+            value={profile.phone}
+            onChange={(e) => updateProfile(e)}
+            name="phone"
+          />
 
-        <Input
-          id="city"
-          placeholder={"ex: New York"}
-          label={"City"}
-          value={profile.city}
-          onChange={(e) => updateProfile(e)}
-          name="city"
-        />
-        <Input
-          id="state"
-          placeholder={"ex: United States"}
-          label={"State / Country"}
-          value={profile.state}
-          onChange={(e) => updateProfile(e)}
-          name="state"
-        />
-        <TextArea
-          id="summary"
-          label={"Summary (optional)"}
-          value={profile.summary}
-          onChange={(e) => updateProfile(e)}
-          name="summary"
-        />
+          <Input
+            id="city"
+            placeholder={"ex: New York"}
+            label={"City"}
+            value={profile.city}
+            onChange={(e) => updateProfile(e)}
+            name="city"
+          />
+          <Input
+            id="state"
+            placeholder={"ex: United States"}
+            label={"State / Country"}
+            value={profile.state}
+            onChange={(e) => updateProfile(e)}
+            name="state"
+          />
+          <TextArea
+            id="summary"
+            label={"Summary (optional)"}
+            value={profile.summary}
+            onChange={(e) => updateProfile(e)}
+            name="summary"
+          />
+        </div>
       </CardContent>
     </Card>
   );
