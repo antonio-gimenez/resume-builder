939fecb0b4884b50be669ce565ff505f374ca426
Antonio Gimenez <grupocapgemini-services.antonio.gimenez@hp.com>
2022-12-15 21:45:36 +0100

    Placement for dropdown

--- src/components/ui/Dropdown.jsx
+++ src/components/ui/Dropdown.jsx
@@ -12,18 +12,21 @@ function Dropdown({ label, items, onClick = () => {}, ...props }) {
     setOpen(false);
   };
 
+  // detect if is on mobile
+  const isMobile = window.innerWidth <= 768 ? true : false;
+
   return (
     <div className="dropdown">
       <button {...props} onClick={handleOpen}>
         {label}
+        <ul className={`menu ${open ? "open" : ""} ${isMobile ? "mobile" : ""}`}>
+          {items.map((child) => (
+            <li key={child.id} className="menu-item" onClick={() => handleClick(child.value)}>
+              {child.label}
+            </li>
+          ))}
+        </ul>
       </button>
-      <ul className={`menu ${open ? "open" : ""}`}>
-        {items.map((child) => (
-          <li key={child.id} className="menu-item" onClick={() => handleClick(child.value)}>
-            {child.label}
-          </li>
-        ))}
-      </ul>
     </div>
   );
 }
