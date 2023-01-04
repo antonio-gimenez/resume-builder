d2a21ab15e2fbf5a3a701818e290ee44343db683
Antonio Gimenez <58783747+antonio-gimenez@users.noreply.github.com>
2023-01-04 11:20:04 +0100

    wip: sections order

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -1,26 +1,10 @@
-import React, { useState } from "react";
-import useResume from "../hooks/useResume";
+import React from "react";
 import initialState from "../data.json";
 import { Input } from "./ui";
 import useSectionTitles from "../reducers/useSectionTitles";
 
 function Form() {
-  const [state, { insertUpdateData }] = useResume(initialState);
-
-  const [sections, { upsertSectionTitle }] = useSectionTitles(initialState.sectionTitles);
-  const [formState, setFormState] = useState(state);
-
-  const handleChange = (event) => {
-    const { name, value } = event.target;
-    setFormState({ ...formState, [name]: value });
-  };
-
-  const handleSubmit = (event) => {
-    event.preventDefault();
-    Object.entries(formState).forEach(([field, value]) => {
-      insertUpdateData(field, value);
-    });
-  };
+  const [sections, { upsertSectionTitle, changeOrder }] = useSectionTitles(initialState.sectionTitles);
 
   return (
     <>
@@ -31,27 +15,19 @@ function Form() {
           id="firstName"
           name="firstName"
           onChange={(event) => {
-            upsertSectionTitle(0, event.target.value);
+            upsertSectionTitle("workExperience", event.target.value);
           }}
-          value={state.sectionTitles["workExperience"]}
+          defaultValue={sections["workExperience"]}
         />
-      </form>
-      <form onSubmit={handleSubmit}>
-        {/* {JSON.stringify(formState)} */}
         <Input
-          label="First Name"
+          label={sections["education"]}
           id="firstName"
           name="firstName"
-          value={formState.firstName || ""}
-          onChange={handleChange}
+          onChange={(event) => {
+            upsertSectionTitle("education", event.target.value);
+          }}
+          defaultValue={sections["education"]}
         />
-        <br />
-        <label htmlFor="email">Email:</label>
-        <input type="email" id="email" name="email" value={formState.email} onChange={handleChange} />
-        <br />
-        <label htmlFor="phone">Street:</label>
-        <input type="text" id="phone" name="phone" value={formState.phone} onChange={handleChange} />
-        <br />
       </form>
     </>
   );
--- src/reducers/useSectionTitles.jsx
+++ src/reducers/useSectionTitles.jsx
@@ -1,34 +1,89 @@
 import { useReducer } from "react";
 
+// example of section titles state
+// sectionTitles = [
+//   { "workExperience": "Experiencia Laboral" },
+// ]
+
 function useSectionTitles(initialState) {
-  const reducer = (state = initialState, action) => {
+  const upsertReducer = (state, action) => {
     switch (action.type) {
-      case "UPSERT_SECTION_TITLE":
-        return Object.assign({}, state, {
-          sectionTitles: Object.assign({}, state.sectionTitles, {
-            [action.sectionId]: action.title,
-          }),
+      case "UPSERT":
+        // If the item already exists in the object, update it
+        if (Object.keys(state).includes(action.payload.sectionName)) {
+          return {
+            ...state,
+            [action.payload.sectionName]: action.payload.title,
+          };
+        }
+        // Otherwise, return a new object with the item added
+        return {
+          ...state,
+          [action.payload.sectionName]: action.payload.title,
+        };
+      case "DELETE":
+        // If the item exists in the object, delete it
+        if (Object.keys(state).includes(action.payload.sectionName)) {
+          const { [action.payload.sectionName]: _, ...newState } = state;
+          return newState;
+        }
+        // Otherwise, return the state as is
+        return state;
+      case "RESET":
+        return initialState[action.payload.sectionName];
+      case "CHANGE_ORDER":
+        const sectionNames = Object.keys(state);
+        const sectionIndex = sectionNames.indexOf(action.payload.sectionName);
+        if (sectionIndex === -1) {
+          return state;
+        }
+        let newSectionNames;
+        if (action.payload.direction === "up") {
+          newSectionNames = [
+            ...sectionNames.slice(0, sectionIndex - 1),
+            sectionNames[sectionIndex],
+            sectionNames[sectionIndex - 1],
+            ...sectionNames.slice(sectionIndex + 1),
+          ];
+        } else if (action.payload.direction === "down") {
+          newSectionNames = [
+            ...sectionNames.slice(0, sectionIndex),
+            sectionNames[sectionIndex + 1],
+            sectionNames[sectionIndex],
+            ...sectionNames.slice(sectionIndex + 2),
+          ];
+        } else {
+          return state;
+        }
+        const newState = {};
+        newSectionNames.forEach((name) => {
+          newState[name] = state[name];
         });
-      case "DELETE_SECTION_TITLE":
-        const newSectionTitles = Object.assign({}, state.sectionTitles);
-        delete newSectionTitles[action.sectionId];
-        return Object.assign({}, state, { sectionTitles: newSectionTitles });
+        return newState;
+
       default:
         return state;
     }
   };
 
-  const [state, dispatch] = useReducer(reducer, initialState);
+  const [state, dispatch] = useReducer(upsertReducer, initialState);
 
-  const upsertSectionTitle = (sectionId, title) => {
-    dispatch({ type: "UPSERT_SECTION_TITLE", sectionId, title });
+  const upsertSectionTitle = (sectionName, title) => {
+    dispatch({ type: "UPSERT", payload: { sectionName, title } });
   };
 
-  const deleteSectionTitle = (sectionId) => {
-    dispatch({ type: "DELETE_SECTION_TITLE", sectionId });
+  const deleteSectionTitle = (sectionName) => {
+    dispatch({ type: "DELETE", payload: { sectionName } });
   };
 
-  return [state, { upsertSectionTitle, deleteSectionTitle }];
+  const resetSectionTitle = (sectionName) => {
+    dispatch({ type: "RESET", payload: { sectionName } });
+  };
+
+  const changeOrder = (sectionName, direction) => {
+    dispatch({ type: "CHANGE_ORDER", payload: { sectionName, direction } });
+  };
+
+  return [state, { upsertSectionTitle, deleteSectionTitle, resetSectionTitle, changeOrder }];
 }
-
 export default useSectionTitles;
